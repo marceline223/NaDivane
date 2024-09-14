56a995521228f4806fde01e6a2f0096bb0779242
Darya Filimonova <dasha220301@gmail.com>
2024-09-14 03:35:43 +0300

    Убрала остатки прошлой версии проекта

--- apps/client/karma.conf.js
+++ apps/client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/split-check'),
+      dir: require('path').join(__dirname, './coverage/na-divane'),
       subdir: '.',
       reporters: [
         { type: 'html' },
